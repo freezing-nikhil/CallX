74713535b3feddceb70b98071a419e85c8c440ce
freezing-nikhil <nsangale443@gmail.com>
2026-01-19 23:23:52 +0530

    Remove sensitive API keys from otp-service.js
    Removed sensitive API keys from the OTP service.

--- CallX/backend/services/otp-service.js
+++ CallX/backend/services/otp-service.js
@@ -1,12 +1,11 @@
 const crypto = require("crypto");
 const hashService = require("./hash-service");
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey("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");
+sgMail.setApiKey("");
 
 
 const twilio = require("twilio")(
-  "AC2f15c951a25db3dc5373af11dd7b3334",
-  "4ddc3804020a61e2bedcae56c3831d55",
+ 
   { lazyLoading: true }
 );
 
@@ -21,7 +20,7 @@ class OtpService {
     const phoneClean = phone.replace(/\D/g, '');
     const message = await twilio.messages.create({
       body: `Your OTP is ${otp} valid for 2 minutes`,
-      messagingServiceSid: "MGb44c98ba494e83d6314b28c6f1cdbc49",
+      messagingServiceSid: "",
       to: `+91${phoneClean}`,
     });
     return message;
